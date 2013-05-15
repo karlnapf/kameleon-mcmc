2fd54f7c905d8dfb72891d050203213402488b84
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-05-15 20:11:50 +0100

    corrected proposal likelihood computation

--- main/mcmc/samplers/MCMCSampler.py
+++ main/mcmc/samplers/MCMCSampler.py
@@ -1,5 +1,6 @@
 from numpy.ma.core import log
 from numpy.matlib import rand
+from numpy.core.numeric import Inf
 
 class MCMCSampler(object):
     def __init__(self, distribution):
@@ -8,6 +9,7 @@ class MCMCSampler(object):
     
     def init(self, start):
         self.current = start
+        self.log_lik_current=-Inf
     
     def update(self, samples, ratios):
         raise NotImplementedError()
@@ -25,32 +27,33 @@ class MCMCSampler(object):
         log_lik - log-likelihood of returned sample
         log_ratio - log probability of acceptance
         """
-        # first step
+        # create proposal around current point in first step only
         if self.Q is None:
             self.Q = self.construct_proposal(self.current)
         
+        # propose sample and construct new Q centred at proposal
         proposal = self.Q.sample(1)
         Q_new = self.construct_proposal(proposal)
+
+        # evaluate both Q
+        log_Q_proposal_given_current = self.Q.log_pdf(proposal)
+        log_Q_current_given_proposal = Q_new.log_pdf(self.current)
         
         log_lik_proposal = self.distribution.log_pdf(proposal)
-        log_lik_current = self.distribution.log_pdf(self.current)
-        log_Q_proposal = self.Q.log_pdf(proposal)
-        log_Q_current = Q_new.log_pdf(self.current)
-        
-        log_ratio = log_lik_proposal - log_Q_proposal - log_lik_current + log_Q_current
+        log_ratio = log_lik_proposal - self.log_lik_current \
+                    + log_Q_current_given_proposal - log_Q_proposal_given_current
         log_ratio = min(log(1), log_ratio)
         
         accepted = log_ratio > log(rand(1))
         
         if accepted:
             sample = proposal
-            log_lik = log_lik_proposal
+            self.log_lik_current = log_lik_proposal
             self.Q = Q_new
         else:
             sample = self.current.copy()
-            log_lik = log_lik_current
             
         # update state: position and proposal
         self.current = sample.copy()
             
-        return sample, proposal, accepted, log_lik, log_ratio
+        return sample, proposal, accepted, self.log_lik_current, log_ratio
