c5e92fe1aa60105fa671e930de97008841fefa7f
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-05-23 18:49:17 +0100

    added path in file

--- main/experiments/scripts/RingResults.py
+++ main/experiments/scripts/RingResults.py
@@ -1,6 +1,10 @@
-from main.experiments.SingleChainAggregator import SingleChainAggregator
 from posixpath import expanduser
 import os
+import sys
+to_add=os.sep.join(os.path.abspath(os.path.dirname(sys.argv[0])).split(os.sep)[0:-3])
+sys.path.append(to_add)
+
+from main.experiments.SingleChainAggregator import SingleChainAggregator
 
 if __name__ == '__main__':
     experiment_dir = expanduser("~") + os.sep + "mcmc_hammer_experiments" + os.sep
