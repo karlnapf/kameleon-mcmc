24bbdc2ae16c8a91917bf5d0a15b2f850cbcd901
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-05-23 19:13:37 +0100

    slight change to syntax

--- main/experiments/scripts/RingResults.py
+++ main/experiments/scripts/RingResults.py
@@ -6,20 +6,18 @@ sys.path.append(to_add)
 from main.experiments.SingleChainAggregator import SingleChainAggregator
 
 if __name__ == '__main__':
-    if len(sys.argv)!=5:
-        print "usage:", str(sys.argv[0]), "<idx_from> <idx_to> <folder> <experiment>"
+    if len(sys.argv)!=4:
+        print "usage:", str(sys.argv[0]), "<idx_from> <idx_to> <folder_base>"
         print "example: python RingResults.py 0 9 ~/mcmc_hammer_experiments MCMCHammerWindow_Ring_"
         exit()
         
     a=int(str(sys.argv[1]))
     b=int(str(sys.argv[2]))
-    folder=str(sys.argv[3])
-    experiment_base=str(sys.argv[4])
+    folder_base=str(sys.argv[3])
     indices=range(a, b+1)
-    print "aggregating experiments", indices, "of", folder + os.sep + experiment_base
+    print "aggregating experiments", indices, "of", folder_base
     
-    experiment_dir = folder + os.sep
-    folders = [experiment_dir + experiment_base + str(i) + os.sep for i in indices]
+    folders = [folder_base + str(i) + os.sep for i in indices]
     ag = SingleChainAggregator(folders)
     ag.load_raw_results()
     ag.post_process()
