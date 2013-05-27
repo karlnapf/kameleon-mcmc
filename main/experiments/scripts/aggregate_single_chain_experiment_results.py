8f3a0392732f34abf49435edc9a4e548ddc0dff2
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-05-27 14:31:27 +0100

    fixed a bug

--- main/experiments/scripts/aggregate_single_chain_experiment_results.py
+++ main/experiments/scripts/aggregate_single_chain_experiment_results.py
@@ -13,7 +13,10 @@ if __name__ == '__main__':
     a=int(str(sys.argv[1]))
     b=int(str(sys.argv[2]))
     folder_base=str(sys.argv[3])
-    output_filename=folder_base + os.sep + "results_" + str(a) + "_" + str(b) + ".txt"
+    
+    temp=folder_base.split(os.sep)
+    output_filename=temp[:-1] + os.sep + temp[-1] + "results_" + str(a) + "_" + str(b) + ".txt"
+    
     indices=range(a, b+1)
     lines=[]
     
