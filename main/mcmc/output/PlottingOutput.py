19ecb33fe19d18297cc7cf802767631ff8b3f188
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-08-04 15:18:36 +0100

    addedn density plot of target based on samples so far

--- main/mcmc/output/PlottingOutput.py
+++ main/mcmc/output/PlottingOutput.py
@@ -10,31 +10,52 @@ Written (W) 2013 Dino Sejdinovic
 
 from main.mcmc.output.Output import Output
 from main.tools.Visualise import Visualise
+from matplotlib.cm import get_cmap
 from matplotlib.pyplot import subplot, plot, xlabel, ylabel, title, hist, show, \
-    draw, clf, figure
+    draw, clf, figure, axis, suptitle
 from numpy.core.numeric import zeros
 from numpy.ma.core import array, exp
 
 class PlottingOutput(Output):
-    def __init__(self, distribution, plot_from=0):
+    def __init__(self, distribution=None, plot_from=0):
         self.distribution=distribution
         self.plot_from = plot_from
-        self.Xs, self.Ys=Visualise.get_plotting_arrays(distribution)
+        
+        if distribution is not None:
+            self.Xs, self.Ys=Visualise.get_plotting_arrays(distribution)
     
     def update(self, mcmc_chain, step_output):
         if mcmc_chain.iteration > self.plot_from:
             subplot(2, 3, 1)
-            Visualise.plot_array(self.Xs, self.Ys, self.P)
+            if self.distribution is not None:
+                Visualise.plot_array(self.Xs, self.Ys, self.P)
             
             samples=mcmc_chain.samples[0:mcmc_chain.iteration]
+            likelihoods=mcmc_chain.log_liks[0:mcmc_chain.iteration]
             proposal_1d=step_output.proposal_object.samples[0,:]
             
             y = samples[len(samples) - 1]
-            plot(samples[:, 0], samples[:, 1], 'm')
+            
+            # plot samples, colored by likeligood
+            normalised=likelihoods.copy()
+            normalised=normalised-normalised.min()
+            normalised=normalised/normalised.max()
+            
+            cm=get_cmap("jet")
+            for i in range(len(samples)):
+                color = cm(normalised[i])
+                plot(samples[i,0], samples[i,1]  ,"o", color=color)       
+            
             plot(y[0], y[1], 'r*', markersize=15.0)
             plot(proposal_1d[0], proposal_1d[1], 'y*', markersize=15.0)
-            Visualise.contour_plot_density(mcmc_chain.mcmc_sampler.Q, self.Xs, \
-                                           self.Ys, log_domain=False)
+            if self.distribution is not None:
+                Visualise.contour_plot_density(mcmc_chain.mcmc_sampler.Q, self.Xs, \
+                                               self.Ys, log_domain=False)
+            else:
+                Visualise.contour_plot_density(mcmc_chain.mcmc_sampler.Q)
+                axis('equal')
+            suptitle(mcmc_chain.mcmc_sampler.__class__.__name__)
+                
             xlabel("$x_1$")
             ylabel("$x_2$")
             title("Samples")
@@ -65,11 +86,13 @@ class PlottingOutput(Output):
             clf()
     
     def prepare(self):
-        figure(figsize=(20, 13))
-        self.P = zeros((len(self.Xs), len(self.Ys)))
-        for i in range(len(self.Xs)):
-            for j in range(len(self.Ys)):
-                x = array([[self.Xs[i], self.Ys[j]]])
-                self.P[j, i] = self.distribution.log_pdf(x)
+        figure(figsize=(18, 10))
         
-        self.P = exp(self.P)
+        if self.distribution is not None:
+            self.P = zeros((len(self.Xs), len(self.Ys)))
+            for i in range(len(self.Xs)):
+                for j in range(len(self.Ys)):
+                    x = array([[self.Xs[i], self.Ys[j]]])
+                    self.P[j, i] = self.distribution.log_pdf(x)
+            
+            self.P = exp(self.P)
