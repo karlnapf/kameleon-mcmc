ef5823c5fbd0d930d3d6d697dcf7397033ef7d7c
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-08-05 16:53:56 +0100

    renamed GPData file

---
+++ main/gp/GPData.py
@@ -0,0 +1,91 @@
+"""
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Written (W) 2013 Heiko Strathmann
+"""
+
+from numpy.core.function_base import linspace
+from numpy.lib.npyio import loadtxt
+from numpy.ma.core import sin, cos, asarray, zeros, array
+from numpy.random import randn, randint, seed, rand
+from scipy.constants.constants import pi
+import os
+
+class GPData(object):
+    @staticmethod
+    def sample_circle_data(n, noise_level=0.025, offset=0.05, seed_init=None):
+        if seed_init is not None:
+            seed(seed_init)
+        
+        # decision surface for sampled data
+        thetas=linspace(0,pi/2,n)
+        X=sin(thetas)*(1+randn(n)*noise_level)
+        Y=cos(thetas)*(1+randn(n)*noise_level)
+        
+        # randomly select labels and distinguish data
+        labels=randint(0,2,n)*2-1
+        idx_a=labels>0
+        idx_b=labels<0
+        X[idx_a]*=(1.+offset)
+        Y[idx_a]*=(1.+offset)
+        X[idx_b]*=(1.-offset)
+        Y[idx_b]*=(1.-offset)
+        
+        return asarray(zip(X,Y)), labels
+    
+    @staticmethod
+    def sample_rectangle_data(n, noise_level=0.015, offset=0.05, seed_init=None):
+        if seed_init is not None:
+            seed(seed_init)
+        
+        # rectangle data
+        a=rand(n/2)*(1-offset)
+        b=rand(n/2)*(1-offset)
+        data=zeros((n,2))
+        labels=zeros(n)
+        for i in range(len(a)):
+            labels[i]=1.0 if rand()>0.5 else -1.0
+            data[i,0]=a[i]
+            data[i,1]+=labels[i]*offset+randn()*noise_level
+            
+        for i in range(len(b)):
+            j=i+len(b)
+            labels[j]=1.0 if rand()>0.5 else -1.0
+            data[j,1]=b[i]
+            data[j,0]+=labels[j]*offset+randn()*noise_level
+            
+        return data, labels
+    
+    @staticmethod
+    def get_pima_data():
+        data_dir=os.sep.join(__file__.split(os.sep)[:-3] + ["data"])
+        filename=data_dir+os.sep+"pima-indians-diabetes.data"
+        data=loadtxt(filename, delimiter=",")
+        
+        # create labelling
+        lab=data[:,-1]
+        lab=array([1. if x==1 else -1.0 for x in lab])
+        
+        # cut off labeling
+        data=data[:,:-1]
+        
+        return data, lab
+    
+    @staticmethod
+    def get_glass_data():
+        data_dir=os.sep.join(__file__.split(os.sep)[:-3] + ["data"])
+        filename=data_dir+os.sep+"glass.data"
+        data=loadtxt(filename, delimiter=",")
+        
+        # create a binary "window glass" vs "non-window glass" labelling
+        lab=data[:,-1]
+        lab=array([1. if x<=4 else -1.0 for x in lab])
+        
+        # cut off ids and labeling
+        data=data[:,1:-1]
+        
+        return data,lab
+
