a9e8043c950cdd37562fe8bfc150319dcf6bd0d0
git <dino.sejdinovic@gmail.com>
2014-01-24 13:57:04 +0000

    estimate MMD

--- main/kernel/Kernel.py
+++ main/kernel/Kernel.py
@@ -10,7 +10,8 @@ Written (W) 2013 Dino Sejdinovic
 
 from abc import abstractmethod
 from numpy import eye
-from numpy.ma.core import shape
+from numpy.ma.core import shape, mean, sqrt
+from numpy.lib.index_tricks import fill_diagonal
 
 class Kernel(object):
     def __init__(self):
@@ -49,3 +50,20 @@ class Kernel(object):
         n = shape(K)[0]
         H = eye(n) - 1.0 / n
         return  1.0 / n * H.dot(K.dot(H))
+    
+    @abstractmethod
+    def estimateMMD(self,sample1,sample2,unbiased=False):
+        """
+        Compute the MMD between two samples
+        """
+        K11 = self.kernel(sample1,sample1)
+        K22 = self.kernel(sample2,sample2)
+        K12 = self.kernel(sample1,sample2)
+        if unbiased:
+            fill_diagonal(K11,0.0)
+            fill_diagonal(K22,0.0)
+            n=float(shape(K11)[0])
+            m=float(shape(K22)[0])
+            return sqrt( sum(sum(K11))/(pow(n,2)-n) + sum(sum(K22))/(pow(m,2)-m) - 2*mean(K12[:]) )
+        else:
+            return sqrt( mean(K11[:])+mean(K22[:])-2*mean(K12[:]) )
