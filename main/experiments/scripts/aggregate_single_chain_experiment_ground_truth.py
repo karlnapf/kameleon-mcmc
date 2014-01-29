15c70732f67f7da5a23ca98fc023790d5ce13de9
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-01-29 17:32:08 +0000

    added script to combine ground truth chains to one iid sample set

---
+++ main/experiments/scripts/aggregate_single_chain_experiment_ground_truth.py
@@ -0,0 +1,48 @@
+"""
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Written (W) 2013 Heiko Strathmann
+Written (W) 2013 Dino Sejdinovic
+"""
+
+import os
+import sys
+
+from main.experiments.GroundTruthSingleChainExperimentAggregator import GroundTruthSingleChainExperimentAggregator
+from main.experiments.SingleChainExperimentAggregator import \
+    SingleChainExperimentAggregator
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print "usage:", str(sys.argv[0]).split(os.sep)[-1], "<idx_from> <idx_to> <folder_base>"
+        print "example:"
+        print "python " + str(sys.argv[0]).split(os.sep)[-1] + " 0 2 /nfs/data3/ucabhst/kameleon_experiments/KameleonWindow_Ring_"
+        exit()
+        
+    a = int(str(sys.argv[1]))
+    b = int(str(sys.argv[2]))
+    folder_base = str(sys.argv[3])
+    
+    temp = folder_base.split(os.sep)
+    output_filename = os.sep.join(temp[:-1]) + os.sep + temp[-1] + "results_" + str(a) + "_" + str(b) + ".txt"
+    
+    indices = range(a, b + 1)
+    lines = []
+    
+    lines.append("aggregating experiments " + str(indices) + " of " + folder_base)
+    print lines[-1]
+    
+    folders = [folder_base + str(i) + os.sep for i in indices]
+    ag = GroundTruthSingleChainExperimentAggregator(folders, thinning_factor=10000)
+    results = ag.aggregate()
+    print os.linesep.join(results)
+    lines += results
+    
+    # save to result file
+    f = open(output_filename, 'w')
+    f.write(os.linesep.join(lines))
+    f.close()
