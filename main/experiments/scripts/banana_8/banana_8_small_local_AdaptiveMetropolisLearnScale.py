040d8a1a1f05e17eeb480252b369706a66608b99
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-01-31 15:57:04 +0000

    local small banana scripts

---
+++ main/experiments/scripts/banana_8/banana_8_small_local_AdaptiveMetropolisLearnScale.py
@@ -0,0 +1,58 @@
+"""
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Written (W) 2013-2014 Heiko Strathmann
+Written (W) 2013 Dino Sejdinovic
+"""
+
+from numpy.lib.twodim_base import eye
+from numpy.ma.core import zeros
+import os
+import sys
+
+from main.distribution.Banana import Banana
+from main.experiments.SingleChainExperiment import SingleChainExperiment
+from main.kernel.GaussianKernel import GaussianKernel
+from main.mcmc.MCMCChain import MCMCChain
+from main.mcmc.MCMCParams import MCMCParams
+from main.mcmc.output.StatisticsOutput import StatisticsOutput
+from main.mcmc.samplers.AdaptiveMetropolis import AdaptiveMetropolis
+from main.mcmc.samplers.AdaptiveMetropolisLearnScale import \
+    AdaptiveMetropolisLearnScale
+from main.mcmc.samplers.KameleonWindowLearnScale import \
+    KameleonWindowLearnScale
+from main.mcmc.samplers.StandardMetropolis import StandardMetropolis
+
+
+if __name__ == '__main__':
+    experiment_dir = str(os.path.abspath(sys.argv[0])).split(os.sep)[-1].split(".")[0] + os.sep
+    
+    distribution = Banana(dimension=8, bananicity=0.03, V=100)
+    sigma = GaussianKernel.get_sigma_median_heuristic(distribution.sample(1000).samples)
+    sigma = 10
+    print "using sigma", sigma
+    kernel = GaussianKernel(sigma=sigma)
+    
+    
+    burnin = 20000
+    num_iterations = 40000
+    
+    #mcmc_sampler = KameleonWindowLearnScale(distribution, kernel, stop_adapt=burnin)
+    mean_est = zeros(distribution.dimension, dtype="float64")
+    cov_est = 1.0 * eye(distribution.dimension)
+    cov_est[0, 0] = distribution.V
+    mcmc_sampler = AdaptiveMetropolisLearnScale(distribution, mean_est=mean_est, cov_est=cov_est)
+    #mcmc_sampler = AdaptiveMetropolis(distribution, mean_est=mean_est, cov_est=cov_est)
+    #mcmc_sampler = StandardMetropolis(distribution)
+        
+    start = zeros(distribution.dimension, dtype="float64")
+    mcmc_params = MCMCParams(start=start, num_iterations=num_iterations, burnin=burnin)
+    
+    mcmc_chain = MCMCChain(mcmc_sampler, mcmc_params)
+    mcmc_chain.append_mcmc_output(StatisticsOutput())
+    
+    experiment = SingleChainExperiment(mcmc_chain, experiment_dir)
+    experiment.run()
