febdebe3378ea28a55185ddf11828cec9fd432a9
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-04-18 11:21:12 +0100

    added multivariate Bernoulli distribution and thorough unit tests

---
+++ kameleon_mcmc/distribution/Bernoulli.py
@@ -0,0 +1,90 @@
+"""
+Copyright (c) 2013-2014 Heiko Strathmann
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ *
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ *
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the author.
+"""
+
+from numpy import asarray, int32
+import numpy
+from numpy import log
+from numpy.matlib import repmat
+from numpy.random import rand
+
+from kameleon_mcmc.distribution.Distribution import Distribution, Sample
+
+
+class Bernoulli(Distribution):
+    def __init__(self, ps):
+        if not type(ps) is numpy.ndarray:
+            raise TypeError("Probability vector must be a numpy array")
+        
+        Distribution.__init__(self, len(ps))
+        
+        if not len(ps) > 0:
+            raise ValueError("Probability vector must contain at least one element")
+        
+        if not all(ps > 0) and all (ps < 1):
+            raise ValueError("Probability vector must lie in (0,1)")
+        
+            
+        self.ps = ps
+    
+    def __str__(self):
+        s = self.__class__.__name__ + "=["
+        s += "ps=" + str(self.ps)
+        s += "]"
+        return s
+    
+    def sample(self, n=1):
+        if not type(n) is int:
+            raise TypeError("Number of samples must be integer")
+
+        if n <= 0:
+            raise ValueError("Number of samples (%d) needs to be positive", n)
+        
+        r = rand(n, self.dimension)
+        s = asarray(r <= self.ps, dtype=int32)
+        return Sample(s)
+    
+    def log_pdf(self, X):
+        if not type(X) is numpy.ndarray:
+            raise TypeError("X must be a numpy array")
+        
+        if not len(X.shape) is 2:
+            raise TypeError("X must be a 2D numpy array")
+        
+        if not X.shape[1] == self.dimension:
+            raise ValueError("Dimension of X does not match own dimension")
+
+        if any(X[X != 0] != 1) or any(X[X != 1] != 0):
+            raise ValueError("X must be zero or one")
+        
+        result=repmat(self.ps, len(X), 1)
+        idx=X==0
+        result[idx]=1-result[idx]
+        
+        return sum(log(result),0)
+
---
+++ kameleon_mcmc/tests/unit/BernoulliUnitTest.py
@@ -0,0 +1,194 @@
+"""
+Copyright (c) 2013-2014 Heiko Strathmann
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ *
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ *
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the author.
+"""
+
+from numpy import zeros, ones, asarray, mean, log
+import numpy
+from numpy.random import rand, randint
+import unittest
+
+from kameleon_mcmc.distribution.Bernoulli import Bernoulli
+from kameleon_mcmc.distribution.Distribution import Sample
+
+
+class BernoulliUnitTest(unittest.TestCase):
+
+    def test_contructor_wrong_ps_type1(self):
+        ps = 0
+        self.assertRaises(TypeError, Bernoulli, ps)
+        
+    def test_contructor_wrong_ps_type2(self):
+        ps = None
+        self.assertRaises(TypeError, Bernoulli, ps)
+
+    def test_contructor_wrong_ps_dim(self):
+        ps = zeros(0)
+        self.assertRaises(ValueError, Bernoulli, ps)
+        
+    def test_contructor_wrong_ps_range_0(self):
+        ps = zeros(0)
+        self.assertRaises(ValueError, Bernoulli, ps)
+        
+    def test_contructor_wrong_ps_range_1(self):
+        ps = ones(0)
+        self.assertRaises(ValueError, Bernoulli, ps)
+        
+    def test_contructor_correct_dim(self):
+        ps = rand(4)
+        b = Bernoulli(ps)
+        self.assertEqual(ps.shape, b.ps.shape)
+        
+    def test_contructor_correct_values(self):
+        ps = rand(4)
+        b = Bernoulli(ps)
+        self.assertTrue(all(ps == b.ps))
+    
+    def test_sample_wrong_n_zero(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(ValueError, b.sample, 0)
+    
+    def test_sample_wrong_n_sameller_zero(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(ValueError, b.sample, -1)
+    
+    def test_sample_wrong_n_type_none(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(TypeError, b.sample, None)
+        
+    def test_sample_wrong_n_type_float(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(TypeError, b.sample, float(1.))
+        
+    def test_sample_result_type(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertEqual(type(b.sample(1)), Sample)
+        
+    def test_sample_dim(self):
+        n = 3
+        d = 2
+        p = asarray(rand(d))
+        b = Bernoulli(p)
+        s = b.sample(n)
+        self.assertEqual(s.samples.shape, (n, d))
+        
+    def test_sample_mean_values(self):
+        n = 10000
+        d = 3
+        runs = 100
+        
+        for _ in range(runs):
+            ps = asarray(rand(d))
+            b = Bernoulli(ps)
+            s = b.sample(n)
+            for i in range(d):
+                self.assertAlmostEqual(mean(s.samples[:, i]), ps[i], delta=0.05)
+                
+    def test_log_pdf_wrong_type_none(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(TypeError, b.log_pdf, None)
+        
+    def test_log_pdf_wrong_type_float(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(TypeError, b.log_pdf, float(1.))
+        
+    def test_log_pdf_wrong_array_dimension_1(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(TypeError, b.log_pdf, zeros(1))
+        
+    def test_log_pdf_wrong_array_dimension_3(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(TypeError, b.log_pdf, zeros(3))
+        
+    def test_log_pdf_wrong_dimension(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(ValueError, b.log_pdf, zeros((1, 2)))
+        
+    def test_log_pdf_wrong_range_minus_1(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(ValueError, b.log_pdf, asarray([[-1]]))
+        
+    def test_log_pdf_wrong_range_minus_2(self):
+        p = asarray([0.5])
+        b = Bernoulli(p)
+        self.assertRaises(ValueError, b.log_pdf, asarray([[2]]))
+    
+    def test_log_pdf_type(self):
+        p = asarray([0.3])
+        b = Bernoulli(p)
+        self.assertEqual(type(b.log_pdf(asarray([[1]]))), numpy.ndarray)
+    
+    def test_log_pdf_array_dimension(self):
+        d = 3
+        n = 2
+        ps = asarray(rand(d))
+        b = Bernoulli(ps)
+        X = randint(0, 2, (n, d))
+        self.assertEqual(b.log_pdf(X).shape, (d,))
+        
+    def test_log_pdf_success_single(self):
+        d = 1
+        ps = asarray(rand(d))
+        b = Bernoulli(ps)
+        X = asarray([[1]])
+        self.assertEqual(b.log_pdf(X), log(ps[0]))
+        
+    def test_log_pdf_failure_single(self):
+        d = 1
+        ps = asarray(rand(d))
+        b = Bernoulli(ps)
+        X = asarray([[0]])
+        self.assertEqual(b.log_pdf(X), log(1 - ps[0]))
+        
+    def test_log_pdf_success_multiple(self):
+        d = 4
+        n = 3
+        ps = asarray(rand(d))
+        b = Bernoulli(ps)
+        X = randint(0, 2, (n, d))
+        
+        # naive computation of log pdf
+        expected = zeros((n, d))
+        for i in range(n):
+            for j in range(d):
+                expected[i, j] = log(ps[j]) if X[i, j] == 1 else log(1 - ps[j])
+                
+        self.assertTrue(all(sum(expected, 0) == b.log_pdf(X)))
+        
+if __name__ == "__main__":
+    unittest.main()
