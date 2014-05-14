1cb982263079243fd53ed7e807fa452eeb37055d
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-05-14 17:35:25 +0100

    working demo of hopfield gibbs sampling

--- kameleon_mcmc/tests/GibbsHopfieldFullConditionalsTest.py
+++ kameleon_mcmc/tests/GibbsHopfieldFullConditionalsTest.py
@@ -8,49 +8,37 @@ Written (W) 2013 Heiko Strathmann
 Written (W) 2013 Dino Sejdinovic
 """
 
+from numpy import zeros, fill_diagonal
 from numpy.random import rand, randn
-from numpy import zeros, fill_diagonal, asarray, arange, reshape, mean
-from matplotlib.pyplot import show, plot, hist
+
 from kameleon_mcmc.distribution.Hopfield import Hopfield
 from kameleon_mcmc.distribution.full_conditionals.HopfieldFullConditionals import HopfieldFullConditionals
+from kameleon_mcmc.mcmc.MCMCChain import MCMCChain
+from kameleon_mcmc.mcmc.MCMCParams import MCMCParams
+from kameleon_mcmc.mcmc.output.DiscretePlottingOutput import DiscretePlottingOutput
+from kameleon_mcmc.mcmc.output.StatisticsOutput import StatisticsOutput
+from kameleon_mcmc.mcmc.samplers.Gibbs import Gibbs
 
 
 def main():
-    d=20
-    b=randn(d)
-    V=randn(d,d)
-    W=V+V.T
-    fill_diagonal(W,zeros(d))
-    full_hopfield=Hopfield(W,b)
-    schedule="in_turns"
-    current_stateA=list(rand(d)<0.5)
-    current_stateB=list(rand(d)<0.5)
-    conditionalsA=HopfieldFullConditionals(full_hopfield, \
-                                          current_stateA, \
-                                          schedule, \
-                                          index_block=arange(d) )
-    conditionalsB=HopfieldFullConditionals(full_hopfield, \
-                                          current_stateB, \
-                                          schedule, \
-                                          index_block=arange(d) )
-    n=500000
-    burnin=1000
-    thin=100
-    X=reshape(zeros(n*d)==1,(n,d))
-    Y=reshape(zeros(n*d)==1,(n,d))
-    for ii in arange(n):
-        #print 'now sampling index: ' + str(conditionals.current_idx)
-        X[ii]=conditionalsA.sample(1)
-        Y[ii]=conditionalsB.sample(1)
-        #print X[ii]
-    hist(full_hopfield.log_pdf(X[arange(burnin,n,thin)]))
-    print mean(X[arange(burnin,n,thin)],0)
-    print mean(Y[arange(burnin,n,thin)],0)
-    show()
+    d = 5
+    b = randn(d)
+    V = randn(d, d)
+    W = V + V.T
+    fill_diagonal(W, zeros(d))
+    hopfield = Hopfield(W, b)
+    current_state = [rand() < 0.5 for _ in range(d)]
+    distribution = HopfieldFullConditionals(full_target=hopfield,
+                                            current_state=current_state)
     
+    mcmc_sampler = Gibbs(distribution)
     
+    start = zeros(distribution.dimension)
+    mcmc_params = MCMCParams(start=start, num_iterations=10000)
+    chain = MCMCChain(mcmc_sampler, mcmc_params)
     
-    
-    
+    chain.append_mcmc_output(StatisticsOutput(plot_times=True, lag=100))
+    chain.append_mcmc_output(DiscretePlottingOutput(plot_from=0, lag=100))
+    chain.run()
     
 main()
