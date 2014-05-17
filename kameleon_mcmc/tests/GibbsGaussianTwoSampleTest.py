e3461af217824ffaa59ea7027ea562d73d087b19
sejdino <dino.sejdinovic@gmail.com>
2014-05-17 01:05:20 +0100

    update to testing script

--- kameleon_mcmc/tests/GibbsGaussianTwoSampleTest.py
+++ kameleon_mcmc/tests/GibbsGaussianTwoSampleTest.py
@@ -26,7 +26,7 @@ The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the author.
 """
-from numpy import zeros, eye, pi, concatenate, array
+from numpy import zeros, eye, pi, concatenate, array, reshape, empty
 
 from kameleon_mcmc.distribution.Gaussian import Gaussian
 from kameleon_mcmc.distribution.full_conditionals.GaussianFullConditionals import GaussianFullConditionals
@@ -36,8 +36,55 @@ from kameleon_mcmc.mcmc.samplers.Gibbs import Gibbs
 from kameleon_mcmc.tools.MatrixTools import MatrixTools
 from kameleon_mcmc.kernel.GaussianKernel import GaussianKernel
 import time
+from kameleon_mcmc.tools.Visualise import Visualise
+from matplotlib.pyplot import show
+from pickle import dump, load
 
-
+def all_tests(gaussian1,gaussian2,n=200):
+    oracle_samples1 = gaussian1.sample(n=n).samples
+    oracle_samples2 = gaussian2.sample(n=n).samples
+    
+    distribution1 = GaussianFullConditionals(gaussian1, list(gaussian1.mu))
+    distribution2 = GaussianFullConditionals(gaussian2, list(gaussian2.mu))
+    
+    mcmc_sampler1 = Gibbs(distribution1)
+    mcmc_sampler2 = Gibbs(distribution2)
+    
+    start = zeros(2)
+    mcmc_params = MCMCParams(start=start, num_iterations=2000+n, burnin=2000)
+    
+    chain1 = MCMCChain(mcmc_sampler1, mcmc_params)
+    chain1.run()
+    gibbs_samples1 = chain1.get_samples_after_burnin()
+    
+    
+    chain2 = MCMCChain(mcmc_sampler2, mcmc_params)
+    chain2.run()
+    gibbs_samples2 = chain2.get_samples_after_burnin()
+    
+    sigma = GaussianKernel.get_sigma_median_heuristic(concatenate((oracle_samples1,oracle_samples2),axis=0))
+    kernel = GaussianKernel(sigma=sigma)
+    
+    vanillap=empty((2,2))
+    blockp=empty((2,2))
+    wildp=empty((2,2))
+    
+    vanillap[0,0]=kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='vanilla')
+    vanillap[0,1]=kernel.TwoSampleTest(oracle_samples1,gibbs_samples2,method='vanilla')
+    vanillap[1,0]=kernel.TwoSampleTest(gibbs_samples1,oracle_samples2,method='vanilla')
+    vanillap[1,1]=kernel.TwoSampleTest(gibbs_samples1,gibbs_samples2,method='vanilla')
+    
+    blockp[0,0]=kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='block')
+    blockp[0,1]=kernel.TwoSampleTest(oracle_samples1,gibbs_samples2,method='block')
+    blockp[1,0]=kernel.TwoSampleTest(gibbs_samples1,oracle_samples2,method='block')
+    blockp[1,1]=kernel.TwoSampleTest(gibbs_samples1,gibbs_samples2,method='block')
+    
+    wildp[0,0]=kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='wild')
+    wildp[0,1]=kernel.TwoSampleTest(oracle_samples1,gibbs_samples2,method='wild')
+    wildp[1,0]=kernel.TwoSampleTest(gibbs_samples1,oracle_samples2,method='wild')
+    wildp[1,1]=kernel.TwoSampleTest(gibbs_samples1,gibbs_samples2,method='wild')
+    
+    return vanillap,blockp,wildp
 def main():
     # covariance has stretched Eigenvalues, and rotated basis
     Sigma1 = eye(2)
@@ -51,83 +98,80 @@ def main():
     Sigma2 = U.T.dot(Sigma2).dot(U)
     
     gaussian1 = Gaussian(Sigma=Sigma1)
-    gaussian2 = Gaussian(mu=array([1, 0]), Sigma=Sigma1)
+    gaussian2 = Gaussian(mu=array([1., 0.]), Sigma=Sigma1)
+    numTrials=4
+    vanillap=empty((numTrials,2,2))
+    blockp=empty((numTrials,2,2))
+    wildp=empty((numTrials,2,2))
     
-    oracle_samples1 = gaussian1.sample(n=200).samples
-    oracle_samples2 = gaussian2.sample(n=200).samples
-    
-    distribution1 = GaussianFullConditionals(gaussian1, [0., 0.])
-    distribution2 = GaussianFullConditionals(gaussian2, [1., 0.])
-    
-    mcmc_sampler1 = Gibbs(distribution1)
-    mcmc_sampler2 = Gibbs(distribution2)
-    
-    start = zeros(2)
-    mcmc_params = MCMCParams(start=start, num_iterations=2200, burnin=2000)
-    
-    chain1 = MCMCChain(mcmc_sampler1, mcmc_params)
-    chain1.run()
-    gibbs_samples1 = chain1.get_samples_after_burnin()
-    
-    chain2 = MCMCChain(mcmc_sampler2, mcmc_params)
-    chain2.run()
-    gibbs_samples2 = chain2.get_samples_after_burnin()
-    
-    sigma = GaussianKernel.get_sigma_median_heuristic(concatenate((oracle_samples1,oracle_samples2),axis=0))
-    kernel = GaussianKernel(sigma=sigma)
-    
-    print '...running the oracle1<->oracle2 tests'
-    print 'mmd (oracle1<->oracle2): ', kernel.estimateMMD(oracle_samples1,oracle_samples2)
-    
-    start=time.time()
-    print 'p-value (vanilla): ', kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='vanilla')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    start=time.time()
-    print 'p-value (block): ', kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='block')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    start=time.time()
-    print 'p-value (wild): ', kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='wild')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    print '...running the oracle1<->gibbs1 tests'
-    print 'mmd (oracle1<->gibbs1): ', kernel.estimateMMD(oracle_samples1,gibbs_samples1)
-    
-    start=time.time()
-    print 'p-value (vanilla): ', kernel.TwoSampleTest(oracle_samples1,gibbs_samples1,method='vanilla')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    start=time.time()
-    print 'p-value (block): ', kernel.TwoSampleTest(oracle_samples1,gibbs_samples1,method='block')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    start=time.time()
-    print 'p-value (wild): ', kernel.TwoSampleTest(oracle_samples1,gibbs_samples1,method='wild')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    print '...running the oracle2<->gibbs1 tests'
-    print 'mmd (oracle2<->gibbs1): ', kernel.estimateMMD(oracle_samples2,gibbs_samples1)
-    
-    start=time.time()
-    print 'p-value (vanilla): ', kernel.TwoSampleTest(oracle_samples2,gibbs_samples1,method='vanilla')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    start=time.time()
-    print 'p-value (block): ', kernel.TwoSampleTest(oracle_samples2,gibbs_samples1,method='block')
-    end=time.time()
-    print 'time elapsed:', end-start
-    
-    start=time.time()
-    print 'p-value (wild): ', kernel.TwoSampleTest(oracle_samples2,gibbs_samples1,method='wild')
-    end=time.time()
-    print 'time elapsed:', end-start
+    #     f = open("/home/dino/git/test_results.dat", "r")
+    #     vanillarej=load(f)
+    #     blockrej=load(f)
+    #     wildrej=load(f)
+    #     f.close()
+    #     
+    for i in range(numTrials):
+        vanillap[i],blockp[i],wildp[i]=all_tests(gaussian1,gaussian2,n=20)
+    f = open("/nfs/home2/dino/git/test_results.dat", "w")
+    vanillarej=sum(vanillap<0.05,0)/numTrials
+    blockrej=sum(blockp<0.05,0)/numTrials
+    wildrej=sum(wildp<0.05,0)/numTrials
+    dump(vanillarej, f)
+    dump(blockrej, f)
+    dump(wildrej, f)
+    f.close()
+#     print '...running the oracle1<->oracle2 tests'
+#     print 'mmd (oracle1<->oracle2): ', kernel.estimateMMD(oracle_samples1,oracle_samples2)
+#     
+#     start=time.time()
+#     print 'p-value (vanilla): ', kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='vanilla')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     start=time.time()
+#     print 'p-value (block): ', kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='block')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     start=time.time()
+#     print 'p-value (wild): ', kernel.TwoSampleTest(oracle_samples1,oracle_samples2,method='wild')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     print '...running the oracle1<->gibbs1 tests'
+#     print 'mmd (oracle1<->gibbs1): ', kernel.estimateMMD(oracle_samples1,gibbs_samples1)
+#     
+#     start=time.time()
+#     print 'p-value (vanilla): ', kernel.TwoSampleTest(oracle_samples1,gibbs_samples1,method='vanilla')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     start=time.time()
+#     print 'p-value (block): ', kernel.TwoSampleTest(oracle_samples1,gibbs_samples1,method='block')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     start=time.time()
+#     print 'p-value (wild): ', kernel.TwoSampleTest(oracle_samples1,gibbs_samples1,method='wild')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     print '...running the oracle2<->gibbs1 tests'
+#     print 'mmd (oracle2<->gibbs1): ', kernel.estimateMMD(oracle_samples2,gibbs_samples1)
+#     
+#     start=time.time()
+#     print 'p-value (vanilla): ', kernel.TwoSampleTest(oracle_samples2,gibbs_samples1,method='vanilla')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     start=time.time()
+#     print 'p-value (block): ', kernel.TwoSampleTest(oracle_samples2,gibbs_samples1,method='block')
+#     end=time.time()
+#     print 'time elapsed:', end-start
+#     
+#     start=time.time()
+#     print 'p-value (wild): ', kernel.TwoSampleTest(oracle_samples2,gibbs_samples1,method='wild')
+#     end=time.time()
+#     print 'time elapsed:', end-start
 
 main()
